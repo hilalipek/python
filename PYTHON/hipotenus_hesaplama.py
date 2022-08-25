4d0d5a4f58d247cdaa89c73e47cc4be88137c476
hilalipek <hilalipk.003@gmail.com>
2022-08-25 19:22:25 +0300

    hipotenus hesabı

---
+++ PYTHON/hipotenus_hesaplama.py
@@ -0,0 +1,6 @@
+import math
+
+a = int(input("a kenarinin uzunlugunu giriniz:"))
+b = int(input("b kenarinin uzunlugunu giriniz:"))
+hipotenus = math.pow(a,2) +  math.pow(b,2)
+print(math.sqrt(hipotenus))
