5c55f5e6c6701f63f4d9267e68b6ef7435cbaf5b
hilalipek <hilalipk.003@gmail.com>
2022-09-11 12:48:32 +0300

    isci_problemi

---
+++ PYTHON/isci_problemi.py
@@ -0,0 +1,6 @@
+birisciissüresi=int(input("bir isci bir isi kac günde bitiriyor"))
+toplamisci=int(input("toplam isci sayisi ne kadar"))
+
+isinbitmesi= birisciissüresi / toplamisci
+
+print("isin bitme süresi",isinbitmesi, "gündür.")
