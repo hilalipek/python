53ba83801a50db5550afa86374a537f37429f0a9
hilalipek <hilalipk.003@gmail.com>
2022-09-13 12:12:13 +0300

    havuz_problemi

---
+++ PYTHON/havuz_problemi.py
@@ -0,0 +1,8 @@
+havuzundolmasi=int(input("havuzun hacmi ne kadar"))
+gereklimusluk=int(input("bir musluk satte ne kadar su akitiyor"))
+
+
+havuz=havuzundolmasi/gereklimusluk
+
+
+print("havuzun bir saatte dolması icin",havuz, "tane musluk gereklidir")
