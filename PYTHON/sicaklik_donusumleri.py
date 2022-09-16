c00380cf3a94240d34696866c92599bca0c30e85
hilalipek <hilalipk.003@gmail.com>
2022-09-16 20:40:31 +0300

    sicaklik_donusumu

---
+++ PYTHON/sicaklik_donusumleri.py
@@ -0,0 +1,11 @@
+#celcius'tan kelvin ve fahreheit donusumu
+
+derece=float (input("dereceyi giriniz"))
+fahrenheit=(derece*180/ 100)+32
+print(derece,"derecenin fahrenheite donusturulmus hali", fahrenheit,"f")
+
+
+
+derece=float (input("dereceyi giriniz"))
+kelvin=(derece+273)
+print(derece,"derecenin kelvine donusturulmus hali",kelvin,"k")
